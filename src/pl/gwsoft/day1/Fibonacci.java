f44faf1410ec3b24df87e92d767288319e3588ca
Grzegorz <grzegorz.wodo@gmail.com>
2018-03-06 19:30:03 +0100

    Add 2 simple class

---
+++ src/pl/gwsoft/day1/Fibonacci.java
@@ -0,0 +1,8 @@
+package pl.gwsoft.day1;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/pl/gwsoft/day1/HelloJava.java
+++ src/pl/gwsoft/day1/HelloJava.java
@@ -3,6 +3,52 @@ package pl.gwsoft.day1;
 public class HelloJava {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        createTriangle();
+        createRectangleDiagonal();
+        createRectangle();
+        createRectangleTwoDiagonals();
+    }
+
+    private static void createTriangle() {
+        System.out.println("");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (j < i) System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void createRectangleDiagonal() {
+        System.out.println("");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == j) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void createRectangle() {
+        System.out.println("");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i == 0 || j == 0 || j == 5 || i == 5) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void createRectangleTwoDiagonals() {
+        System.out.println("");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (j==i || j==(5-i)) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println("");
+        }
     }
 }
